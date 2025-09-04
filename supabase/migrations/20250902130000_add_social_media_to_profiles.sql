4d09e88d3262cb76c22ddea3e01c26276841c403
jacob <JAKOUBBAKHOUCHE50@GMAIL.COM>
2025-09-04 14:33:54 +0200

    Delete supabase/migrations/20250902130000_add_social_media_to_profiles.sql

--- supabase/migrations/20250902130000_add_social_media_to_profiles.sql
+++
@@ -1,6 +0,0 @@
--- supabase/migrations/20250902130000_add_social_media_to_profiles.sql
-
-ALTER TABLE public.profiles
-ADD COLUMN social_media JSONB DEFAULT '{}'::jsonb;
-
-COMMENT ON COLUMN public.profiles.social_media IS 'Stores social media links like Facebook, Instagram, etc.';
