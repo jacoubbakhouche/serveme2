1ba97ea8685816bdc020ab58c87a569ed040265e
jacob <JAKOUBBAKHOUCHE50@GMAIL.COM>
2025-09-17 15:43:07 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
- import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
